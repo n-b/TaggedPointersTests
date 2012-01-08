aa4baf2cdc49272d0fadd3a0b9028a014b907421
Nicolas Bouilleaud <nicolas.bouilleaud@gmail.com>
2012-01-08 23:36:45 +0100

    Clearing up testing

--- TaggedPointersTests.m
+++ TaggedPointersTests.m
@@ -8,46 +8,58 @@
 
 #import <Foundation/Foundation.h>
 #import <SenTestingKit/SenTestingKit.h>
-#import <CoreData/CoreData.h>
+
+/****************************************************************************/
+#pragma mark Platform checks
+
+@interface TaggedPointersTests : SenTestCase 
+@end
+@implementation TaggedPointersTests
+
+- (void) setUp
+{
+    [super setUp];
+#if ! defined(__x86_64__)
+    STFail(@"These tests are only valid in 64-bit");
+#endif
+}
+@end
 
 /****************************************************************************/
 #pragma mark NSNumber Tests
 
-@interface TaggedPointersTests_NSNumber : SenTestCase
+@interface TaggedPointersTests_NSNumber : TaggedPointersTests
 @end
 @implementation TaggedPointersTests_NSNumber
 
-- (void) testNSNumberInt16
+- (void) testSanity
 {
-    short value = (short)random();
-    NSNumber * obj1 = [NSNumber numberWithShort:value];
-    NSNumber * obj2 = [NSNumber numberWithShort:value];
-    
+    NSNumber * obj1 = [NSNumber numberWithInt:42];
+    NSNumber * obj2 = [NSNumber numberWithInt:42];
     STAssertEqualObjects(obj1, obj2, @"both objects are equal");
-    STAssertEquals(obj1, obj2, @"both objects are **the same**");
-    STAssertEquals(value, (short)((long)obj1>>8), @"the pointer is the value");
 }
 
-- (void) testNSNumberInt32
+- (void) testPointerEquality
 {
-    int value = (int)random();
-    NSNumber * obj1 = [NSNumber numberWithInt:value];
-    NSNumber * obj2 = [NSNumber numberWithInt:value];
-    
-    STAssertEqualObjects(obj1, obj2, @"both objects are equal");
+    NSNumber * obj1 = [NSNumber numberWithInt:42];
+    NSNumber * obj2 = [NSNumber numberWithInt:42];
     STAssertEquals(obj1, obj2, @"both objects are **the same**");
-    STAssertEquals(value, (int)((long)obj1>>8), @"the pointer is the value");
 }
 
-- (void) testNSNumberInt64
+- (void) testPointerValue
 {
-    long value = random(); // long is the same as long long in 64-bit
-    NSNumber * obj1 = [NSNumber numberWithLong:value];
-    NSNumber * obj2 = [NSNumber numberWithLong:value];
-    
-    STAssertEqualObjects(obj1, obj2, @"both objects are equal");
-    STAssertEquals(obj1, obj2, @"both objects are **the same**");
-    STAssertEquals(value, (long)((long)obj1>>8), @"the pointer is the value");
+    NSNumber * object = [NSNumber numberWithInt:42];
+    // shifting the last byte reveals the value
+    STAssertEquals(42, (int)((long)object>>8), @"the value is in the pointer");
+}
+
+- (void) testMonkey
+{
+    for (int i=0; i<1000; i++) {
+        int value = (int)random();
+        NSNumber * object = [NSNumber numberWithInt:value];
+        STAssertEquals(value, (int)((long)object>>8), @"the value is in the pointer");
+    }
 }
 
 @end
@@ -55,7 +67,7 @@
 /****************************************************************************/
 #pragma mark NSDate tests
 
-@interface TaggedPointersTests_NSDate : SenTestCase
+@interface TaggedPointersTests_NSDate : TaggedPointersTests
 @end
 
 @implementation TaggedPointersTests_NSDate
@@ -65,72 +77,21 @@ typedef union _DoupleOrPointer {
     void * p;
 } DoupleOrPointer;
 
-- (void) testNSDateSinceReference
+- (void) testPointerValue
 {
-    DoupleOrPointer dop = {.interval = 0.0};
-    
+    DoupleOrPointer dop = {.interval = 42.0};
     NSDate * object = [NSDate dateWithTimeIntervalSinceReferenceDate:dop.interval];
-
-    // Masking out the last 4 bits gives the passed interval
-    STAssertEquals(dop.p, (void*)((long)object&0xFFFFFFFFFFFFFFF0), @"the pointer is the value");
+    // Masking out the last byte gives the passed interval
+    STAssertEquals(dop.p, (void*)((long)object&0xFFFFFFFFFFFFFF00), @"the pointer is the value");
 }
 
-@end
-
-
-
-/****************************************************************************/
-#pragma mark CoreData Tests
-
-@interface TaggedPointersTests_NSManagedObjectID : SenTestCase
-@end
-
-@implementation TaggedPointersTests_NSManagedObjectID
-
-- (void) testCoreData
+- (void) testMonkey
 {
-    return;
-    // It seems CoreData does not (yet) use, at least externally, tagged pointers for NSManagedObjectIDs
-    // This code is left here for future reference.
-    
-    NSEntityDescription * entity = [NSEntityDescription new];
-    entity.name = @"entity";
-    NSAttributeDescription * attribute = [NSAttributeDescription new];
-    attribute.attributeType = NSInteger64AttributeType;
-    attribute.name = @"attribute";
-    [entity setProperties:[NSArray arrayWithObject:attribute]];
-    NSManagedObjectModel * model = [NSManagedObjectModel new];
-    model.entities = [NSArray arrayWithObject:entity];
-    NSPersistentStoreCoordinator * coordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:model];
-    NSError * error = nil;
-    [coordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:[NSURL fileURLWithPath:@"/tmp/test.sqlite"] options:nil error:&error];
-    NSManagedObjectContext * context = [NSManagedObjectContext new];
-    context.persistentStoreCoordinator = coordinator;
-    
-    
-    NSFetchRequest * request = [NSFetchRequest new];
-    request.entity = entity;
-    NSArray * array = [context executeFetchRequest:request error:&error];
-    for (NSManagedObject * object in array) {
-        NSManagedObjectID * objectID = [object objectID];
-        printf("objectID : %p (%d)\n",objectID,(int)objectID>>8);
-    }
-    
-    
     for (int i=0; i<1000; i++) {
-        NSManagedObject * object = [NSEntityDescription insertNewObjectForEntityForName:@"entity" inManagedObjectContext:context];
-        [object setValue:[NSNumber numberWithInt:i] forKey:@"attribute"];
-    }
-    [context save:&error];
-    
-    request = [NSFetchRequest new];
-    request.entity = entity;
-    request.resultType = NSManagedObjectIDResultType;
-    array = [context executeFetchRequest:request error:&error];
-    for (NSManagedObjectID * objectID in array) {
-        printf("objectID : %p (%d)\n",objectID,(int)objectID>>8);
+        DoupleOrPointer dop = {.interval = (double)random()};
+        NSDate * object = [NSDate dateWithTimeIntervalSinceReferenceDate:dop.interval];
+        // Masking out the last byte gives the passed interval
+        STAssertEquals(dop.p, (void*)((long)object&0xFFFFFFFFFFFFFF00), @"the pointer is the value");
     }
 }
-
-
 @end
